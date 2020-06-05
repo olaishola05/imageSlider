bc1f2b461286e06564f7b27ff6f4375eca6733fe
Ola Ishola <olaishola@hotmail.co.uk>
2020-06-05 22:46:49 +0100

    fixed the blank photo

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-img_arr = ["img/travel.jpeg", "img/city.jpeg", "img/sky.jpeg", "img/road.jpeg", "img/simple.jpeg", "img/dontkno.jpeg"];
+img_arr = ["img/city.jpeg", "img/sky.jpeg", "img/road.jpeg", "img/simple.jpeg", "img/dontkno.jpeg", "img/mac.jpeg"];
 let num = 0;
 
 //next button
 function nextButton() {
     let slider = document.getElementById("slide-image");
     num++;
-    if (num > img_arr.length) {
+    if (num >= img_arr.length) {
         num = 0;
     }
 
@@ -26,22 +26,13 @@ function previousButton() {
 // display on interval
 function displayInterval() {
     let slider = document.getElementById("slide-image");
-    let i;
-    for (i = 1; i < img_arr.length; i++) {
-        slider.src = img_arr[i];
-
-        if (num > img_arr.length) {
-            num = 0;
-        }
-    }
     num++;
 
-    if (num < 0) {
-        num = img_arr.length - 1;
+    if (num >= img_arr.length) {
+        num = 0;
     }
     slider.src = img_arr[num];
 }
-
 setInterval(displayInterval, 6000);
 
 document.getElementById("next").addEventListener("click", nextButton);
